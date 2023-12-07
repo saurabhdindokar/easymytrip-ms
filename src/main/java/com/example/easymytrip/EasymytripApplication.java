b1415caafc3f514fa7f017f54be2207267dbdb7b
saurabhdindokar <sirdindokar22@gmail.com>
2023-12-07 13:03:34 +0530

    coderefactor

---
+++ src/main/java/com/example/easymytrip/EasymytripApplication.java
@@ -0,0 +1,13 @@
+package com.example.easymytrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasymytripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasymytripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/easymytrip/EasymytripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.easymytrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasymytripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
